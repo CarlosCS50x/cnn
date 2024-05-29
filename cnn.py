37ddf13fb3094f5bd831c40f6845a8d244e549f7
CarlosCS50x <vaultbancario@gmail.com>
2024-05-29 21:27:42 +0200

    update GUI

--- cnn.py
+++ cnn.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import ttk
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -53,20 +55,69 @@ lr_reduction = ReduceLROnPlateau(monitor='val_loss',
                                  factor=0.5, 
                                  min_lr=0.00001)
 
-# Train the model with data augmentation
-history = model.fit(datagen.flow(x_train, y_train, batch_size=32),
-                    epochs=50, 
-                    validation_data=(x_test, y_test),
-                    callbacks=[lr_reduction])
+class PlotLosses(tf.keras.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.fig, self.ax = plt.subplots(1, 2, figsize=(10, 5))
+        self.ax[0].set_xlabel('Epoch')
+        self.ax[0].set_ylabel('Loss')
+        self.ax[0].set_title('Loss Plot')  # Add title to the loss plot
+        self.ax[1].set_xlabel('Epoch')
+        self.ax[1].set_ylabel('Accuracy')
+        self.ax[1].set_title('Accuracy Plot')  # Add title to the accuracy plot
+        self.train_loss = []
+        self.val_loss = []
+        self.train_acc = []
+        self.val_acc = []
+        self.epoch = 0
+        self._model = None  # Note: use a private attribute
 
-# Evaluate the model
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label='val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0, 1])
-plt.legend(loc='lower right')
-plt.show()
+    @property
+    def model(self):
+        return self._model
 
-test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
-print(f'Test accuracy: {test_acc}')
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.epoch += 1
+        self.train_loss.append(logs.get('loss'))
+        self.val_loss.append(logs.get('val_loss'))
+        self.train_acc.append(logs.get('accuracy'))
+        self.val_acc.append(logs.get('val_accuracy'))
+
+        self.ax[0].cla()
+        self.ax[0].plot(range(1, self.epoch + 1), self.train_loss, label='Train')
+        self.ax[0].plot(range(1, self.epoch + 1), self.val_loss, label='Validation')
+        self.ax[0].legend()
+        self.ax[0].set_title('Loss Plot')  # Update title of the loss plot
+        
+        self.ax[1].cla()
+        self.ax[1].plot(range(1, self.epoch + 1), self.train_acc, label='Train')
+        self.ax[1].plot(range(1, self.epoch + 1), self.val_acc, label='Validation')
+        self.ax[1].legend()
+        self.ax[1].set_title('Accuracy Plot')  # Update title of the accuracy plot
+
+        plt.tight_layout()
+        plt.pause(0.1)  # Pause to allow GUI to update
+
+
+# Create an instance of the PlotLosses class
+plot_losses = PlotLosses()
+plot_losses.model = model
+# Create a simple Tkinter GUI
+root = tk.Tk()
+root.title('Training Progress')
+
+def train_model():
+    # Train the model with data augmentation
+    history = model.fit(datagen.flow(x_train, y_train, batch_size=32),
+                        epochs=50, 
+                        validation_data=(x_test, y_test),
+                        callbacks=[lr_reduction, plot_losses])
+
+train_button = ttk.Button(root, text='Train Model', command=train_model)
+train_button.pack(pady=10)
+
+root.mainloop()
